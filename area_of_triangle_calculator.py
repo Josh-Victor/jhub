aa8a7e70de9334d6f99060e0f502d593e2ad3ae9
Josh-Victor <54108933+Josh-Victor@users.noreply.github.com>
2019-09-03 14:13:23 +0200

    Add files via upload

---
+++ area_of_triangle_calculator.py
@@ -0,0 +1,6 @@
+print("Please identify 3 sides of the triangle.")
+triangle_height = float(input("Please enter the measurements for the height of the triangle: "))
+triangle_base = float(input("Please neter the measurements for the base of the triangle: "))
+
+triangle_area = triangle_height * triangle_base / 2
+print("The area of the triangle is " + str(triangle_area))
